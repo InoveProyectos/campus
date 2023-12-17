21188e1fb2d303e0f359b326a1ba7a133b02b901
Fdvalls <fernandodanielvalls@gmail.com>
2023-12-17 18:04:43 -0300

    Boton logout, pantalla recoveryPasswd, se emprolijo un poco los estilos responsivos en las cards

--- src/views/RecoveryPasswd.jsx
+++ src/views/RecoveryPasswd.jsx
@@ -1,32 +1,56 @@
 import React from "react";
+import styles from "./RecoveryPasswd.module.css";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function RecoveryPasswd() {
+  const navigate = useNavigate();
+
+  const handleRecovery = () => {
+    navigate("/login");
+  };
+
   return (
     <>
-      <div>
-        <h1>Solicitar usuario y contraseña</h1>
-        <h3>
-          Ingrese su email de alumno, el sistema le enviará un mail con los
-          datos de sus credenciales nuevas (usuario y contraseña)
-        </h3>
-      </div>
-      <article style={{ display: "flex", flexDirection: "column" }}>
-        <form>
-          <div style={{marginLeft: "-110px"}}>
-            <label htmlFor="email">Correo Electrónico:</label>
-          </div>
-            <input style={{width: "250px", height: "30px", background: "white"}} type="email" id="email" name="email" required />
-          <br />
-          <br />
-          <div style={{marginLeft: "-160px"}}>
-            <label htmlFor="password">Contraseña:</label>
-          </div>
-            <input style={{width: "250px", height: "30px", background: "white"}} type="password" id="password" name="password" required />
-          <br />
-          <br />
-          <button type="submit">Recuperar</button>
-        </form>
-      </article>
+      <section className={styles.bodyRecovery}>
+        <div>
+          <h1>Solicitar usuario y contraseña</h1>
+        </div>
+        <article>
+          <h3>
+            Ingrese su email de alumno, el sistema le enviará un mail con los
+            datos de sus credenciales nuevas (usuario y contraseña)
+          </h3>
+          <form>
+            <div>
+              <label htmlFor="email">Correo Electrónico:</label>
+            </div>
+            <input
+              className={styles.label}
+              type="email"
+              id="email"
+              name="email"
+              color="black"
+            />
+            <br />
+            <br />
+            <div>
+              <label htmlFor="password">Contraseña:</label>
+            </div>
+            <input
+              className={styles.label}
+              type="password"
+              id="password"
+              name="password"
+              color="black"
+            />
+            <br />
+            <br />
+            <div className={styles.button}>
+              <button onClick={() => handleRecovery()} type="submit">Recuperar</button>
+            </div>
+          </form>
+        </article>
+      </section>
     </>
   );
 }
