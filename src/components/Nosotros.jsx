84db612f1d6314b2cc72459c0bebb0efe3bcc6ea
FdValls <fernandodanielvalls@gmail.com>
2024-02-26 19:35:53 -0300

    Pantalla nosotros V2

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -2,24 +2,31 @@ import React from 'react';
 import CardNosotros from './Cards/CardNosotros';
 import backgroundCardNosotros from "../assets/backgorund-cardNosotros.png";
 import logoCompleto from "../assets/logo_completo_color.png";
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    min-height: 100vh;
+    background-image: url(${backgroundCardNosotros});
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;  // Fija la imagen de fondo
+  }
+`;
 
 const AppContainer = styled('div')({
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    backgroundImage: `url(${backgroundCardNosotros})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    minHeight: '100vh',
+    // position: 'relative',
+    minHeight: '100%',
     overflow: 'hidden',
 });
 
 const NavBar = styled('nav')({
-    position: 'initial',
+    position: 'absolute',  // Cambiado de 'relative' a 'absolute'
+    top: 20,  // Ajusta según sea necesario
     left: 20,
-    width: '15%',
     zIndex: 1000,
     marginTop: '20px',
     alignSelf: 'flex-start',
@@ -33,7 +40,7 @@ const NavBar = styled('nav')({
 });
 
 const Container = styled('div')({
-    marginTop: "1%",
+    marginTop: "10%",
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
@@ -45,31 +52,32 @@ const Container = styled('div')({
         textAlign: "center",
         padding: '10px',
         paddingRight: '1px',
-        left: 0,
-        right: 0,
+        marginTop: "200px",
+        justifyContent: 'center',
     },
 });
 
 function Nosotros() {
     return (
-        <AppContainer style={{backgroundImage: `url(${backgroundCardNosotros})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
-            <NavBar><img src={logoCompleto} style={{ width: 150, height: 100 }} alt="" /></NavBar>
-            <Container>
-                <CardNosotros />
-                <CardNosotros />
-                <CardNosotros />
-                <CardNosotros />
-                <CardNosotros />
-                <CardNosotros />
-                <CardNosotros />
-                <CardNosotros />
-                <CardNosotros />
-                <CardNosotros />
-                <CardNosotros />
-                <CardNosotros />
-                <CardNosotros />
-            </Container>
-        </AppContainer>
+        <>
+            <GlobalStyle />
+            <AppContainer>
+                <NavBar><img src={logoCompleto} style={{ width: 150, height: 100 }} alt="" /></NavBar>
+                <Container>
+                    <CardNosotros />
+                    <CardNosotros />
+                    <CardNosotros />
+                    <CardNosotros />
+                    <CardNosotros />
+                    <CardNosotros />
+                    <CardNosotros />
+                    <CardNosotros />
+                    <CardNosotros />
+                    <CardNosotros />
+                    <CardNosotros />
+                </Container>
+            </AppContainer>
+        </>
     );
 }
 
