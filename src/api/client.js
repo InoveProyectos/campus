7703e47183739303cb9dde17e8880ef6a698b458
hernancontigiani <hernan4790@gmail.com>
2024-12-05 00:33:42 -0300

    Handle http protocol

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = "https://admin.inovecode.com"
+const baseURL = `${window.location.protocol}//admin.inovecode.com`
 //const baseURL = "http://127.0.0.1"
 
 export const client = axios.create({
